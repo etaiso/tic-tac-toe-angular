d8def7464d3911f57163593932e2644a78c39025
Etai <etai87@gmail.com>
2018-04-05 18:21:47 +0300

    calculate win_numbers from matrix

--- src/app/services/game-logic/game-logic.service.ts
+++ src/app/services/game-logic/game-logic.service.ts
@@ -8,19 +8,7 @@ export class GameLogicService {
   // | 8,   16,  32 |
   // | 64, 128, 256 |
   // ----------------
-
-  private WIN_NUMBERS = [
-    1 | 2 | 4,
-    8 | 16 | 32,
-    64 | 128 | 256,
-    1 | 8 | 64,
-    2 | 16 | 128,
-    4 | 32 | 256,
-    1 | 16 | 256,
-    4 | 16 | 64
-  ];
-
-  private WIN_MAP = [
+  private WIN_MATRIX = [
     [1, 2, 4],
     [8, 16, 32],
     [64, 128, 256],
@@ -28,15 +16,19 @@ export class GameLogicService {
     [2, 16, 128],
     [4, 32, 256],
     [1, 16, 256],
-    [4,16, 64]
+    [4, 16, 64]
   ];
+
+  // WIN_NUMBERS is the calculated value of each array in the WIN_MATRIX.
+  // the calculated value is an aggregation of the OR operation (bitwsie).
+  private WIN_NUMBERS = this.WIN_MATRIX.map(a => a.reduce((acc, cur) => acc | cur))
 
   constructor() {}
 
   checkForWin(tilesValue: number): number[] {
     const winNumberIndex = this.WIN_NUMBERS.findIndex(num => (num & tilesValue) == num);
     if (winNumberIndex >= 0) {
-      return this.WIN_MAP[winNumberIndex];
+      return this.WIN_MATRIX[winNumberIndex];
     }
   }
 }
