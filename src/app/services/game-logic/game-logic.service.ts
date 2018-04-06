8635ae1c7ea0b20ecbd6a2629cf29c7bb8239ae1
Etai <etai87@gmail.com>
2018-04-06 16:17:11 +0300

    add comments + better names

--- src/app/services/game-logic/game-logic.service.ts
+++ src/app/services/game-logic/game-logic.service.ts
@@ -8,26 +8,29 @@ export class GameLogicService {
   // | 8,   16,  32 |
   // | 64, 128, 256 |
   // ----------------
-  private WIN_MATRIX = [
+  private WINNING_LINES = [
+    // horizontal lines
     [1, 2, 4],
     [8, 16, 32],
     [64, 128, 256],
+    // vertical lines
     [1, 8, 64],
     [2, 16, 128],
     [4, 32, 256],
+    // diagonal lines
     [1, 16, 256],
     [4, 16, 64]
   ];
 
-  // WIN_AGGREGATED_VALUES is the calculated value of each array in the WIN_MATRIX.
+  // WIN_AGGREGATED_VALUES is the calculated value of each array in the WINNING_LINES.
   // the calculated value is an aggregation with OR operation (bitwsie).
   // For e.g: [1, 2, 4] will be reduced to 7 (which is the result of 1 | 2 | 4).
-  private WIN_AGGREGATED_VALUES = this.WIN_MATRIX.map(a => a.reduce((acc, cur) => acc | cur))
+  private WIN_AGGREGATED_VALUES = this.WINNING_LINES.map(a => a.reduce((acc, cur) => acc | cur))
 
   constructor() {}
 
-  checkForWin(tilesValue: number): number[] {
-    const winNumberIndex = this.WIN_AGGREGATED_VALUES.findIndex(num => (num & tilesValue) == num);
-    return winNumberIndex >= 0 ? this.WIN_MATRIX[winNumberIndex] : [];
+  checkForWin(tilesAggregatedValue: number): number[] {
+    const winNumberIndex = this.WIN_AGGREGATED_VALUES.findIndex(val => (val & tilesAggregatedValue) == val);
+    return winNumberIndex >= 0 ? this.WINNING_LINES[winNumberIndex] : [];
   }
 }
